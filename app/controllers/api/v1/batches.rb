4ab29323a0f90408bd5376b1d65544a2663da183
Schwarzenegger <schwarzeneggeral@gmail.com>
2019-04-20 09:14:28 -0300

    Add close and sent batch endpoints

--- app/controllers/api/v1/batches.rb
+++ app/controllers/api/v1/batches.rb
@@ -5,26 +5,60 @@ module API
 
       resource :batches do
 
-      desc "create a new batch"
-      params do
-        requires :purchase_channel_id, type: Integer
-      end
-      post "/" do
-        authenticate_admin!
-        orders = Order.where(purchase_channel_id: params[:purchase_channel_id]).ready
-        if orders.empty?
-          error!(I18n.t('api.batches.invalid_pc'), 400)
-        else
-          batch = Batch.create
-          orders.each do |order|
-            order.batch = batch
-            order.start_production
+        desc "create a new batch"
+        params do
+          requires :purchase_channel_id, type: Integer
+        end
+        post "/" do
+          authenticate_admin!
+          orders = Order.where(purchase_channel_id: params[:purchase_channel_id]).ready
+          if orders.empty?
+            error!(I18n.t('api.batches.invalid_pc'), 400)
+          else
+            batch = Batch.create
+            orders.each do |order|
+              order.batch = batch
+              order.start_production
+            end
+
+            render batch
+          end
+        end
+
+        desc "Close a batch"
+        params do
+          requires :id, type: Integer
+        end
+        put ":id/close" do
+          authenticate_admin!
+          batch = Batch.find(params[:id])
+          orders_closed = []
+          batch.orders.each do |order|
+            if order.finish_product
+              orders_closed << order
+            end
           end
 
-          render batch
+          render orders_closed
         end
-      end
 
+        desc "Sent a batch to clients"
+        params do
+          requires :id, type: Integer
+          requires :delivery_service, type: Integer
+        end
+        put ":id/sent" do
+          authenticate_admin!
+          orders_sent = []
+          batch = Batch.find(params[:id])
+          batch.orders.where(delivery_service: params[:delivery_service]).each do |order|
+            if order.deliver
+              orders_sent << order
+            end
+          end
+
+          render orders_sent
+        end
       end
     end
   end
--- spec/controllers/api/v1/batches_spec.rb
+++ spec/controllers/api/v1/batches_spec.rb
@@ -42,4 +42,81 @@ describe API::V1::Batches, type: :request do
       end
     end
   end
+
+  context "PUT" do
+    describe ":id/close" do
+      it "Return forbidden if has no token" do
+        put '/api/batches/1/close'
+
+        expect(response.status).to eq(401)
+      end
+
+      it "Return forbidden if token is invalid" do
+        put '/api/batches/1/close', headers: { 'Authorization' => "Token token=asdadadssdas" }
+        expect(response.status).to eq(401)
+      end
+
+      it "should rescue from cant find id" do
+        put '/api/batches/12312321312/close', headers: mount_header(admin_user.access_token)
+        expect(response.status).to eq(404)
+      end
+
+      it "Should close orders from a batch" do
+        new_batch = create(:batch)
+        pc = create(:purchase_channel)
+
+        new_order = create(:order, purchase_channel: pc, status: 2, batch: new_batch)
+        new_order_2 = create(:order, purchase_channel: pc, status: 2, batch: new_batch)
+
+        put "/api/batches/#{new_batch.id}/close", headers: mount_header(admin_user.access_token)
+        expect(response.status).to eq(200)
+        new_order.reload
+        new_order_2.reload
+        expect(new_order.status).to eq "closing"
+        expect(new_order_2.status).to eq "closing"
+      end
+    end
+
+    describe ":id/sent" do
+      it "Return forbidden if has no token" do
+        put '/api/batches/1/sent', params: { delivery_service: 1 }
+
+        expect(response.status).to eq(401)
+      end
+
+      it "Return return 400 if missing param" do
+        put '/api/batches/1/sent', params: {}
+
+        expect(response.status).to eq(400)
+      end
+
+      it "Return forbidden if token is invalid" do
+        put '/api/batches/1/sent', params: { delivery_service: 1 }, headers: { 'Authorization' => "Token token=asdadadssdas" }
+        expect(response.status).to eq(401)
+      end
+
+      it "should rescue from cant find id" do
+        put '/api/batches/12312321312/sent', params: { delivery_service: 1 }, headers: mount_header(admin_user.access_token)
+        expect(response.status).to eq(404)
+      end
+
+      it "Should sent orders from a batch to clients with chosen delivery service" do
+        new_batch = create(:batch)
+        pc = create(:purchase_channel)
+
+        new_order = create(:order, purchase_channel: pc, status: 3, batch: new_batch, delivery_service: 1)
+        new_order_2 = create(:order, purchase_channel: pc, status: 3, batch: new_batch, delivery_service: 1)
+        new_order_3 = create(:order, purchase_channel: pc, status: 3, batch: new_batch, delivery_service: 2)
+
+        put "/api/batches/#{new_batch.id}/sent", params: { delivery_service: 1 }, headers: mount_header(admin_user.access_token)
+        expect(response.status).to eq(200)
+        new_order.reload
+        new_order_2.reload
+        new_order_3.reload
+        expect(new_order.status).to eq "sent"
+        expect(new_order_2.status).to eq "sent"
+        expect(new_order_3.status).to eq "closing"
+      end
+    end
+  end
 end
